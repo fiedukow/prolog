d2bacc002b5bf7516d55c6b70b8e029a680b1d31
fiedukow <fiedukow@gmail.com>
2013-01-21 19:43:32 +0100

    Created project merge file - not tested yet. Moved candidate to candidate.pl.

---
+++ candidate.pl
@@ -0,0 +1,159 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%            INDUKCJA REGUŁ - PROJEKT JPS                 %%
+%%          Implementacja funkcji Candidate                %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                                         %%
+%%   Autorzy:                                              %%
+%%      -> Marcin Bożek                                    %%
+%%      -> Andrzej Fiedukowicz                             %%
+%%      -> Maciej Grzybek                                  %%
+%%      -> Edward Miedziński                               %%
+%%                                                         %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Tymczasowa funkcja pokazujaca wszystkich kandydatow dla danej wartosci
+% ilosci dotychczas uzytych zmiennych.
+showCandidates(LastVar) :-
+  findall(Cand/LastVarAfter, candidate(LastVar, Cand, LastVarAfter), List),
+  writeList(List), nl.
+
+% Zwraca kolejnych kandydatow z zalozeniem ze do  tej pory uzyto LastVar zmiennych rule_var 
+% candidate(+LastVar, -Candidate, -NewValueOfLastVar)
+candidate(LastVar, Candidate, LastVarAfter) :-
+  setof(X/ArgNo, R^(op(R), functor(R,X,ArgNo)), Relations),
+  member(Relation, Relations),
+  generateCandidate(LastVar,Relation,Candidate,LastVarAfter).
+
+% Funkcja pomocnicza wypisujaca kolejne elementy listy w nowych liniach
+% writeList(+List)
+writeList([]).
+
+writeList([E | Lista]) :-
+  write(E), nl,
+  writeList(Lista).
+  
+generateCandidate(0, _, _, _) :-
+  !, write('Tak byc nie moze!'), nl,
+  fail.
+
+%przeklada relacje na ilosc jej arguementow bowiem zeby wygenerowac mozliwe dopasowania zmiennych
+%nie trzeba wiedziec do jakiej relacji dopasowywujemy ale tylko ile argumentow jest do dopasowania
+%oraz ile zmiennych zostalo uzytych do tej pory (licznik).
+%generateCandidate(+LastVar, +Relacja/ArgNo, -Cand, -NewValueOfLastVar)
+generateCandidate(LastVar, Relacja/ArgNo, Cand, LastVarAfter) :-
+  generateCandidateArguments(LastVar, ArgNo, Dopasowanie, LastVarAfter),
+  Cand =.. [Relacja|Dopasowanie].
+
+%generuje liste rule_varow o wielkosci ArgNo (lista argumentow dla danego kandydata)
+%generateCandidateArguments(+LastVar, +ArgNo, -Dopasowanie, -NewValueOfLastVar)
+generateCandidateArguments(LastVar, ArgNo, Dopasowanie, LastVarAfter) :-
+  constructOldList(ArgNo, LastVar, Stare), 
+  length(Stare, IleStarych),
+  IleNowych is ArgNo - IleStarych,
+  constructNList(IleNowych, LastVar, Nowe, LastVarAfter), 
+  appendRandom(Nowe, Stare, Dopasowanie).
+  
+%konstruuje liste zmiennych o dlugosci mniejszej lub rownej MaxLenght
+%ta funkcja NIE może dodawać swoich zmiennych, może tylko wykorzystywać stare
+%constructOldList(+MaxLenght, +LastVar, -ListOfVariables)
+constructOldList(0, _,  []) :- !, fail.
+
+constructOldList(MaxLenght, VarOld, Lista) :-
+  constructNListOld(MaxLenght, VarOld, Lista).
+
+constructOldList(MaxLenght, Zmienne, Lista) :-
+  MaxLenght1 is MaxLenght - 1, 
+  constructOldList(MaxLenght1, Zmienne, Lista).
+
+%konstruuje liste zmiennych o dlugosci rownej Length
+%ta funkcja NIE może dodawać swoich zmiennych, może tylko wykorzystywać stare
+%constructNListOld(+Length, +LastVar, -ListOfVariables)
+constructNListOld(0, _, []) :- !.
+
+constructNListOld(N, VarOld, [Var | List]) :-
+  N1 is N - 1, 
+  getVarBetween(1, VarOld, Var),
+  constructNListOld(N1, VarOld, List).
+
+%tworzy kolejne zmienne z zakresu od Beg do End (wlaczajac Beg i End)
+%getVarBetween(+Beg,+End,-Var)
+getVarBetween(Var,_,rule_var(Var)).
+
+getVarBetween(Beg,End,Var) :-
+  Beg < End,
+  Beg1 is Beg + 1,
+  getVarBetween(Beg1,End,Var).
+
+%Tworzy liste zmiennych dlugosci N korzystajac tylko z nowych zmiennych
+%W szczegolnosci nowe zmienne moga byc wykorzystane wielokrotnie
+%Zwraca nowa wartosc LastVar!
+%constructNList(+N, +LastVar, -List, -NewValueOfLastVar)
+constructNList(N, LastVar, List, LastVarEnd) :-
+  constructNList(N, LastVar, LastVar, List, LastVarEnd).
+
+%Tworzy liste zmiennych dlugosci N korzystajac tylko z nowych zmiennych
+%W szczegolnosci nowe zmienne moga byc wykorzystane wielokrotnie
+%Zwraca nowa wartosc LastVar.
+%Wersja z dodatkowym argumentem LastVarAfter ktory jest inkrementowany
+%przy przejsciach przez wersje funkcji dodajaca nowe zmienne
+%a na koniec przepisywany jako LastVarEnd.
+%Z tej funkcji nalezy korzystac przez zaslepke 4ro argumentowa
+%constructNList(+N, +LastVar, +LastVarAfter, -List, -LastVarEnd).
+constructNList(0, _, X, [], X) :- !.
+
+%wersja dodajaca nowa zmienna do aktualnej listy
+constructNList(N, LastVar, LastVarAfter, [rule_var(LastVarAfter1) | List], LastVarEnd) :-
+  N1 is N - 1,
+  LastVarAfter1 is LastVarAfter + 1,
+  constructNList(N1, LastVar, LastVarAfter1, List, LastVarEnd).
+
+%wersja korzystajaca z nowo dodanych zmiennych ale nie dodajaca wlasnych
+constructNList(N, LastVar, LastVarAfter, [X | List], LastVarEnd) :-
+  LastVarAfter > LastVar,
+  N1 is N - 1, 
+  Beg is LastVar + 1,
+  End is LastVarAfter,
+  getVarBetween(Beg, End, X),
+  constructNList(N1, LastVar, LastVarAfter, List, LastVarEnd).
+
+
+%Losowe laczenie dwoch list z zachowaniem kolejnosci w obrebie elementow
+%pochodzacych z list pierwotnych
+%np [a,b], [c,d] -> [a,b,c,d], [a,c,b,d], [a,c,d,b], [c,d,a,b], [c,a,d,b], [c,a,b,d]
+%appendRandom(+L1, +L2, -OutList)
+appendRandom([], Lista2, Lista2) :- !.
+appendRandom(Lista1, [], Lista1) :- !.
+
+appendRandom([P | Lista1Rest], Lista2, [P | RestWynik]) :-
+  appendRandom(Lista1Rest, Lista2, RestWynik).
+
+appendRandom(Lista1, [P | Lista2Rest], [P | RestWynik]) :-
+  appendRandom(Lista1, Lista2Rest, RestWynik).
+  
+  
+op(ojciec(swirski, franek)).
+op(matka(dominika, franek)).
+op(kobieta(dominika)).
+op(mezczyzna(swirski)).
+op(ojciec(franek, darek)).
+op(ojciec(franek, iga)).
+op(matka(gosia, darek)).
+op(matka(gosia, iga)).
+op(mezczyzna(franek)).
+op(rodzic(franek, darek)).
+op(rodzic(franek, iga)).
+op(rodzic(gosia, darek)).
+op(rodzic(gosia, iga)).
+op(brat(darek, iga, ktos)).
+op(brat(darek, iga, kto)).
+op(siostra(iga, darek)).
+op(rodzenstwo(iga, darek)).
+op(rodzenstwo(darek, iga)).
+op(kobieta(iga)).
+op(mezczyzna(darek)).
+
+example(pos(dziadek(swirski, iga))).
+example(pos(dziadek(swirski, darek))).
+example(pos(babcia(dominika, iga))).
+example(pos(babcia(dominika, darek))).
+
---
+++ project.pl
@@ -0,0 +1,137 @@
+% Figure 18.11  A program that induces if-then rules.
+
+% Learning of simple if-then rules
+:- [candidate].
+:-  op(300, xfx, <==).
+
+% learn(Class): collect learning examples into a list, construct and
+% output a description for Class, and assert the corresponding rule about Class
+learn(Class)  :-
+   bagof(example(ClassX, Obj), example(ClassX, Obj), Examples),        % Collect examples
+   learn(Examples, Class, Description),                                  % Induce rule   
+   nl, write(Class), write('  <== '), nl,                                % Output rule   
+   writelist(Description),
+   assert(Class  <==  Description).                                      % Assert rule
+
+% learn(Examples, Class, Description):
+%    Description covers exactly the examples of class Class in list Examples
+
+learn(Examples, Class, [])  :-
+   not(member(example(Class, _ ), Examples)).               % No example to cover 
+
+learn(Examples, Class, [Conj | Conjs])  :-
+   learn_conj(Examples, Class, Conj),
+   remove(Examples, Conj, RestExamples),                    % Remove examples that match Conj   
+   learn(RestExamples, Class, Conjs).                       % Cover remaining examples 
+
+% learn_conj(Examples, Class, Conj):
+%    Conj is a list of attribute values satisfied by some examples of class Class and
+%    no other class
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+myLearn(Relation, Conj) :-
+  Relation =.. [Rel | _],
+  findall(example(X), (example(X), X=..[_,EX], EX=..[Rel|_]), Examples),
+  learn_conj(Examples, Relation, Conj).
+
+%Check if there is any negative example in examples list connected with Relation
+%negativeExample(+Examples, +Relation)
+negativeExample(Examples, Relation) :-
+  Relation =.. [RelationName | _],
+  member(example(neg(NegExample)), Examples),
+  NegExample =.. [RelationName | _].
+
+%Podaj tylko Example dla relacji ktora cie interesuje!!!
+%learn_conj(+Examples, +Relation, -Conj).
+learn_conj(Examples, Relation, Conj) :-
+  functor(Relation, Relation, LastVar),
+  learn_conj(Examples, Relation, LastVar, [], Conj). 
+
+
+%zamkniecie rekurencji przepisuje zebrana liste na wyjscie.
+learn_conj(Examples, Relation, _, Conj, Conj)  :-
+   not(negativeExample(Examples, Relation)), !.   % There is no negative example
+                                                  % TODO is there any covered positive 
+
+learn_conj(Examples, Relation, LastVar, ConjCurrent, Conj)  :-
+   choose_cond(Examples, Relation, LastVar, ConjCurrent, Cond, NewLastVar),    % choose one cond using score   
+   filter(Examples, [Cond | ConjCurrent], NewLastVar, Examples1),   % filter Examples using what 
+                                                                    % you already created
+   learn_conj(Examples1, Relation, NewLastVar, [Cond | ConjCurrent], Conj).   % 
+
+%choose_cond(+Examples, +Relation, +LastVar, +ConjCurrent, -Cond, -NewLastVar)
+choose_cond(Examples, Relation, LastVar, ConjCurrent, Cond, NewLastVar)  :-
+   findall(CondCand/NewLastVar/Score, score(Examples, Relation, LastVar, ConjCurrent, CondCand, Score, NewLastVar), CondCands),
+   best(CondCands, Cond/NewLastVar).                                 % Best score attribute value 
+
+best([AttVal/LV/_], AttVal/LV).
+
+best([AV0/LV0/S0, AV1/LV1/S1 | AVSlist], AttVal)  :-
+   S1  >  S0, !,                                             % AV1 better than AV0   
+   best([AV1/LV1/S1 | AVSlist], AttVal)
+   ;
+   best([AV0/LV0/S0 | AVSlist], AttVal).
+
+
+% filter(Examples, Condition, Examples1):
+%    Examples1 contains elements of Examples that satisfy Condition
+filter(Examples, ConjCand, LastVar, Examples1)  :-
+   findall(example(UEX),
+           (member(example(UEX), Examples), UEX=..[_,EX], satisfy(LastVar,EX,ConjCand)),
+           Examples1).
+
+% remove(Examples, Conj, Examples1):
+%    removing from Examples those examples that are covered by Conj gives Examples1
+
+%remove([], _, []).
+
+%remove([example(Class, Obj) | Es], Conj, Es1)  :-
+%   satisfy(Obj, Conj), !,                                     % First example matches Conj   
+%   remove(Es, Conj, Es1).                                     % Remove it 
+
+%remove([E | Es], Conj, [E | Es1])  :-                         % Retain first example   
+%   remove(Es, Conj, Es1).
+
+%score(+Examples, +Relation, +LastVar, +ConjCurrent, -CondCand, -Score, -NewLastVar)
+score(Examples, Relation, LastVar, ConjCurrent, CondCand, Score, NewLastVar)  :-
+   candidate(LastVar, CondCand, NewLastVar),
+   filter(Examples, [CondCand | ConjCurrent], NewLastVar, Examples1),      % Examples1 satisfy condition Att = Val     
+   length(Examples1, N1),                       % Length of list   
+   count_pos(Examples1, NPos1),          % Number of positive examples   
+   NPos1 > 0,                                    % At least one positive example matches AttVal
+   Score is 2 * NPos1 - N1.
+
+%suitable(AttVal, Examples, Class)  :-            
+%    % At least one negative example must not match AttVal
+%   member(example(ClassX, ObjX), Examples),
+%   ClassX \== Class,                                           % Negative example   
+%   not(satisfy(ObjX, [AttVal])).                           % that does not match 
+
+% count_pos(Examples, Class, N):
+%    N is the number of positive examples of Class
+
+count_pos([], 0).
+
+count_pos([example(X) | Examples], N)  :-
+   count_pos(Examples, N1),
+   (functor(X, pos, _), !, N is N1 + 1; N = N1).
+
+
+writelist([]).
+
+writelist([X | L])  :-
+   tab(2), write(X), nl,
+   writelist(L).
+
+%====== EXAMPLES ======
+
+example(classA,[colour=blue,size=small]).
+example(classB,[colour=green,size=big]).
+example(classB,[colour=blue,size=big]).
+example(classA,[colour=green,size=medium]).
+example(classC,[colour=green,size=small]).
+
+
+
+attribute(colour,[blue,green]).
+attribute(size,[small,big,medium]).
