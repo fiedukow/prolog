a5dff48442acd408981259e11fef19eb5aeea3f7
fiedukow <fiedukow@gmail.com>
2013-01-22 11:37:38 +0100

    Added some examples.

--- examples.pl
+++ examples.pl
@@ -11,8 +11,8 @@ op(rodzic(franek, darek)).
 op(rodzic(franek, iga)).
 op(rodzic(gosia, darek)).
 op(rodzic(gosia, iga)).
-op(brat(darek, iga, ktos)).
-op(brat(darek, iga, kto)).
+op(brat(darek, iga)).
+op(brat(darek, iga)).
 op(siostra(iga, darek)).
 op(rodzenstwo(iga, darek)).
 op(rodzenstwo(darek, iga)).
@@ -23,4 +23,26 @@ example(pos(dziadek(swirski, iga))).
 example(pos(dziadek(swirski, darek))).
 example(pos(babcia(dominika, iga))).
 example(pos(babcia(dominika, darek))).
+example(neg(dziadek(swirski,swirski))).
+example(neg(dziadek(swirski,iga))).
+example(neg(dziadek(swirski,darek))).
+example(neg(dziadek(swirski,dominika))).
+example(neg(dziadek(iga,iga))).
+example(neg(dziadek(iga,darek))).
+example(neg(dziadek(iga,dominika))).
+example(neg(dziadek(darek,darek))).
+example(neg(dziadek(darek,dominika))).
+example(neg(dziadek(dominika,dominika))).
+example(neg(babcia(swirski,swirski))).
+example(neg(babcia(swirski,iga))).
+example(neg(babcia(swirski,darek))).
+example(neg(babcia(swirski,dominika))).
+example(neg(babcia(iga,iga))).
+example(neg(babcia(iga,darek))).
+example(neg(babcia(iga,dominika))).
+example(neg(babcia(darek,darek))).
+example(neg(babcia(darek,dominika))).
+example(neg(babcia(dominika,dominika))).
+
+people([swirski, franek, dominika, iga, gosia, darek]).
 
