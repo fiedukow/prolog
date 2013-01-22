2ca703ba6df21f8ca36dbaa113c43f0455a12236
fiedukow <fiedukow@gmail.com>
2013-01-22 19:46:27 +0100

    Added officicial test case.

---
+++ examples1.pl
@@ -0,0 +1,265 @@
+op(mezczyzna(kondzio)).
+op(mezczyzna(mateusz)).
+op(mezczyzna(wojtek)).
+op(mezczyzna(grzegorz)).
+op(mezczyzna(jan)).
+op(mezczyzna(michal)).
+op(mezczyzna(darek)).
+op(mezczyzna(tomek)).
+op(kobieta(zofia)).
+op(kobieta(michalina)).
+op(kobieta(joanna)).
+op(kobieta(ilona)).
+op(kobieta(krysia)).
+op(kobieta(gosia)).
+op(kobieta(kasia)).
+op(rodzic(kondzio, mateusz)).
+op(rodzic(kondzio, wojtek)).
+op(rodzic(gosia, mateusz)).
+op(rodzic(gosia, wojtek)).
+op(rodzic(jan, michalina)).
+op(rodzic(jan, joanna)).
+op(rodzic(zofia, michalina)).
+op(rodzic(zofia, joanna)).
+op(rodzic(grzegorz, ilona)).
+op(rodzic(michalina, ilona)).
+op(rodzic(wojtek, krysia)).
+op(rodzic(wojtek, tomek)).
+op(rodzic(ilona, krysia)).
+op(rodzic(ilona, tomek)).
+op(rodzic(tomek, michal)).
+op(rodzic(tomek, darek)).
+op(rodzic(joanna, michal)).
+op(rodzic(joanna, darek)).
+
+example(pos(dziadek(kondzio, krysia))).
+example(pos(dziadek(kondzio, tomek))).
+example(pos(dziadek(wojtek, michal))).
+example(pos(dziadek(wojtek, darek))).
+example(pos(dziadek(grzegorz, krysia))).
+example(pos(dziadek(grzegorz, tomek))).
+example(pos(dziadek(jan, ilona))).
+example(pos(dziadek(jan, michal))).
+example(pos(dziadek(jan, darek))).
+
+example(neg(dziadek(kondzio,kondzio))).
+example(neg(dziadek(kondzio,gosia))).
+example(neg(dziadek(kondzio,jan))).
+example(neg(dziadek(kondzio,zofia))).
+example(neg(dziadek(kondzio,grzegorz))).
+example(neg(dziadek(kondzio,michalina))).
+example(neg(dziadek(kondzio,mateusz))).
+example(neg(dziadek(kondzio,wojtek))).
+example(neg(dziadek(kondzio,ilona))).
+example(neg(dziadek(kondzio,joanna))).
+example(neg(dziadek(kondzio,michal))).
+example(neg(dziadek(kondzio,darek))).
+example(neg(dziadek(kondzio,kasia))).
+example(neg(dziadek(gosia,kondzio))).
+example(neg(dziadek(gosia,gosia))).
+example(neg(dziadek(gosia,jan))).
+example(neg(dziadek(gosia,zofia))).
+example(neg(dziadek(gosia,grzegorz))).
+example(neg(dziadek(gosia,michalina))).
+example(neg(dziadek(gosia,mateusz))).
+example(neg(dziadek(gosia,wojtek))).
+example(neg(dziadek(gosia,ilona))).
+example(neg(dziadek(gosia,krysia))).
+example(neg(dziadek(gosia,tomek))).
+example(neg(dziadek(gosia,joanna))).
+example(neg(dziadek(gosia,michal))).
+example(neg(dziadek(gosia,darek))).
+example(neg(dziadek(gosia,kasia))).
+example(neg(dziadek(jan,kondzio))).
+example(neg(dziadek(jan,gosia))).
+example(neg(dziadek(jan,jan))).
+example(neg(dziadek(jan,zofia))).
+example(neg(dziadek(jan,grzegorz))).
+example(neg(dziadek(jan,michalina))).
+example(neg(dziadek(jan,mateusz))).
+example(neg(dziadek(jan,wojtek))).
+example(neg(dziadek(jan,krysia))).
+example(neg(dziadek(jan,tomek))).
+example(neg(dziadek(jan,joanna))).
+example(neg(dziadek(jan,kasia))).
+example(neg(dziadek(zofia,kondzio))).
+example(neg(dziadek(zofia,gosia))).
+example(neg(dziadek(zofia,jan))).
+example(neg(dziadek(zofia,zofia))).
+example(neg(dziadek(zofia,grzegorz))).
+example(neg(dziadek(zofia,michalina))).
+example(neg(dziadek(zofia,mateusz))).
+example(neg(dziadek(zofia,wojtek))).
+example(neg(dziadek(zofia,ilona))).
+example(neg(dziadek(zofia,krysia))).
+example(neg(dziadek(zofia,tomek))).
+example(neg(dziadek(zofia,joanna))).
+example(neg(dziadek(zofia,michal))).
+example(neg(dziadek(zofia,darek))).
+example(neg(dziadek(zofia,kasia))).
+example(neg(dziadek(grzegorz,kondzio))).
+example(neg(dziadek(grzegorz,gosia))).
+example(neg(dziadek(grzegorz,jan))).
+example(neg(dziadek(grzegorz,zofia))).
+example(neg(dziadek(grzegorz,grzegorz))).
+example(neg(dziadek(grzegorz,michalina))).
+example(neg(dziadek(grzegorz,mateusz))).
+example(neg(dziadek(grzegorz,wojtek))).
+example(neg(dziadek(grzegorz,ilona))).
+example(neg(dziadek(grzegorz,joanna))).
+example(neg(dziadek(grzegorz,michal))).
+example(neg(dziadek(grzegorz,darek))).
+example(neg(dziadek(grzegorz,kasia))).
+example(neg(dziadek(michalina,kondzio))).
+example(neg(dziadek(michalina,gosia))).
+example(neg(dziadek(michalina,jan))).
+example(neg(dziadek(michalina,zofia))).
+example(neg(dziadek(michalina,grzegorz))).
+example(neg(dziadek(michalina,michalina))).
+example(neg(dziadek(michalina,mateusz))).
+example(neg(dziadek(michalina,wojtek))).
+example(neg(dziadek(michalina,ilona))).
+example(neg(dziadek(michalina,krysia))).
+example(neg(dziadek(michalina,tomek))).
+example(neg(dziadek(michalina,joanna))).
+example(neg(dziadek(michalina,michal))).
+example(neg(dziadek(michalina,darek))).
+example(neg(dziadek(michalina,kasia))).
+example(neg(dziadek(mateusz,kondzio))).
+example(neg(dziadek(mateusz,gosia))).
+example(neg(dziadek(mateusz,jan))).
+example(neg(dziadek(mateusz,zofia))).
+example(neg(dziadek(mateusz,grzegorz))).
+example(neg(dziadek(mateusz,michalina))).
+example(neg(dziadek(mateusz,mateusz))).
+example(neg(dziadek(mateusz,wojtek))).
+example(neg(dziadek(mateusz,ilona))).
+example(neg(dziadek(mateusz,krysia))).
+example(neg(dziadek(mateusz,tomek))).
+example(neg(dziadek(mateusz,joanna))).
+example(neg(dziadek(mateusz,michal))).
+example(neg(dziadek(mateusz,darek))).
+example(neg(dziadek(mateusz,kasia))).
+example(neg(dziadek(wojtek,kondzio))).
+example(neg(dziadek(wojtek,gosia))).
+example(neg(dziadek(wojtek,jan))).
+example(neg(dziadek(wojtek,zofia))).
+example(neg(dziadek(wojtek,grzegorz))).
+example(neg(dziadek(wojtek,michalina))).
+example(neg(dziadek(wojtek,mateusz))).
+example(neg(dziadek(wojtek,wojtek))).
+example(neg(dziadek(wojtek,ilona))).
+example(neg(dziadek(wojtek,krysia))).
+example(neg(dziadek(wojtek,tomek))).
+example(neg(dziadek(wojtek,joanna))).
+example(neg(dziadek(wojtek,kasia))).
+example(neg(dziadek(ilona,kondzio))).
+example(neg(dziadek(ilona,gosia))).
+example(neg(dziadek(ilona,jan))).
+example(neg(dziadek(ilona,zofia))).
+example(neg(dziadek(ilona,grzegorz))).
+example(neg(dziadek(ilona,michalina))).
+example(neg(dziadek(ilona,mateusz))).
+example(neg(dziadek(ilona,wojtek))).
+example(neg(dziadek(ilona,ilona))).
+example(neg(dziadek(ilona,krysia))).
+example(neg(dziadek(ilona,tomek))).
+example(neg(dziadek(ilona,joanna))).
+example(neg(dziadek(ilona,michal))).
+example(neg(dziadek(ilona,darek))).
+example(neg(dziadek(ilona,kasia))).
+example(neg(dziadek(krysia,kondzio))).
+example(neg(dziadek(krysia,gosia))).
+example(neg(dziadek(krysia,jan))).
+example(neg(dziadek(krysia,zofia))).
+example(neg(dziadek(krysia,grzegorz))).
+example(neg(dziadek(krysia,michalina))).
+example(neg(dziadek(krysia,mateusz))).
+example(neg(dziadek(krysia,wojtek))).
+example(neg(dziadek(krysia,ilona))).
+example(neg(dziadek(krysia,krysia))).
+example(neg(dziadek(krysia,tomek))).
+example(neg(dziadek(krysia,joanna))).
+example(neg(dziadek(krysia,michal))).
+example(neg(dziadek(krysia,darek))).
+example(neg(dziadek(krysia,kasia))).
+example(neg(dziadek(tomek,kondzio))).
+example(neg(dziadek(tomek,gosia))).
+example(neg(dziadek(tomek,jan))).
+example(neg(dziadek(tomek,zofia))).
+example(neg(dziadek(tomek,grzegorz))).
+example(neg(dziadek(tomek,michalina))).
+example(neg(dziadek(tomek,mateusz))).
+example(neg(dziadek(tomek,wojtek))).
+example(neg(dziadek(tomek,ilona))).
+example(neg(dziadek(tomek,krysia))).
+example(neg(dziadek(tomek,tomek))).
+example(neg(dziadek(tomek,joanna))).
+example(neg(dziadek(tomek,michal))).
+example(neg(dziadek(tomek,darek))).
+example(neg(dziadek(tomek,kasia))).
+example(neg(dziadek(joanna,kondzio))).
+example(neg(dziadek(joanna,gosia))).
+example(neg(dziadek(joanna,jan))).
+example(neg(dziadek(joanna,zofia))).
+example(neg(dziadek(joanna,grzegorz))).
+example(neg(dziadek(joanna,michalina))).
+example(neg(dziadek(joanna,mateusz))).
+example(neg(dziadek(joanna,wojtek))).
+example(neg(dziadek(joanna,ilona))).
+example(neg(dziadek(joanna,krysia))).
+example(neg(dziadek(joanna,tomek))).
+example(neg(dziadek(joanna,joanna))).
+example(neg(dziadek(joanna,michal))).
+example(neg(dziadek(joanna,darek))).
+example(neg(dziadek(joanna,kasia))).
+example(neg(dziadek(michal,kondzio))).
+example(neg(dziadek(michal,gosia))).
+example(neg(dziadek(michal,jan))).
+example(neg(dziadek(michal,zofia))).
+example(neg(dziadek(michal,grzegorz))).
+example(neg(dziadek(michal,michalina))).
+example(neg(dziadek(michal,mateusz))).
+example(neg(dziadek(michal,wojtek))).
+example(neg(dziadek(michal,ilona))).
+example(neg(dziadek(michal,krysia))).
+example(neg(dziadek(michal,tomek))).
+example(neg(dziadek(michal,joanna))).
+example(neg(dziadek(michal,michal))).
+example(neg(dziadek(michal,darek))).
+example(neg(dziadek(michal,kasia))).
+example(neg(dziadek(darek,kondzio))).
+example(neg(dziadek(darek,gosia))).
+example(neg(dziadek(darek,jan))).
+example(neg(dziadek(darek,zofia))).
+example(neg(dziadek(darek,grzegorz))).
+example(neg(dziadek(darek,michalina))).
+example(neg(dziadek(darek,mateusz))).
+example(neg(dziadek(darek,wojtek))).
+example(neg(dziadek(darek,ilona))).
+example(neg(dziadek(darek,krysia))).
+example(neg(dziadek(darek,tomek))).
+example(neg(dziadek(darek,joanna))).
+example(neg(dziadek(darek,michal))).
+example(neg(dziadek(darek,darek))).
+example(neg(dziadek(darek,kasia))).
+example(neg(dziadek(kasia,kondzio))).
+example(neg(dziadek(kasia,gosia))).
+example(neg(dziadek(kasia,jan))).
+example(neg(dziadek(kasia,zofia))).
+example(neg(dziadek(kasia,grzegorz))).
+example(neg(dziadek(kasia,michalina))).
+example(neg(dziadek(kasia,mateusz))).
+example(neg(dziadek(kasia,wojtek))).
+example(neg(dziadek(kasia,ilona))).
+example(neg(dziadek(kasia,krysia))).
+example(neg(dziadek(kasia,tomek))).
+example(neg(dziadek(kasia,joanna))).
+example(neg(dziadek(kasia,michal))).
+example(neg(dziadek(kasia,darek))).
+example(neg(dziadek(kasia,kasia))).
+
+
+people([kondzio, gosia, jan, zofia, grzegorz, michalina, mateusz, wojtek, ilona, krysia, tomek, joanna, michal, darek, kasia]).
+
+ruleVarLimit(3).
