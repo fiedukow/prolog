eba72d264169ca55d6790a18bb306c8b42dd09eb
Maciej 'mac' Grzybek <mashester@gmail.com>
2013-01-24 10:36:45 +0100

    Set of extra examples.

---
+++ examples2.pl
@@ -0,0 +1,7 @@
+op(ojciec(marek,edek)).
+op(ojciec(edek,jozek)).
+op(matka(marta,jozek)).
+op(matka(alicja,marta)).
+
+example(pos(przodek2(marek,jozek))). % dziadek(marek,jozek)
+example(pos(przodek2(alicja,jozek))). % babcia(alicja,jozek)
