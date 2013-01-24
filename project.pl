03f6d38aa303b517c21aee30414f08eda45b433a
fiedukow <fiedukow@gmail.com>
2013-01-24 10:55:41 +0100

    Some view improvments in goLearn.

--- project.pl
+++ project.pl
@@ -11,16 +11,22 @@
 goLearn(Relation, Conjs) :-
    Relation =.. [Rel | _],
    findall(example(pos(X)), (example(pos(X)), X=..[Rel|_]), ExamplesPos),
-   learn(Relation, ExamplesPos, Conjs).
+   learn(Relation, ExamplesPos, Conjs),
+   show_rule(Relation, Conjs).
+
+show_rule(_,[]).
+show_rule(Relation, [Conj|Conjs]) :-
+  write(Relation), write(' <= '), write(Conj), nl,
+  show_rule(Relation, Conjs).
 
 negativeExamples(Relation, Examples) :-
    Relation =.. [Rel | _],
    findall(example(neg(X)), (example(neg(X)), X=..[Rel|_]), Examples).
 
 
-remove(Examples, Conj, RestExamples) :-
+remove(Examples, Conj, LastVar, RestExamples) :-
    findall(example(UEX),
-           (member(example(UEX), Examples), UEX=..[_,EX], not(satisfy(5,EX,Conj))),
+           (member(example(UEX), Examples), UEX=..[_,EX], not(satisfy(LastVar,EX,Conj))),
            RestExamples).
 
 % learn(Examples, Class, Description):
@@ -33,22 +39,22 @@ learn(_, [], []) :- !.
 learn(Relation, Examples, [Conj | Conjs])  :-
    negativeExamples(Relation, NegExamples),
    append(Examples, NegExamples, AllExamples),
-   myLearn(Relation, AllExamples, Conj),
-   remove(Examples, Conj, RestExamples),                    % Remove examples that match Conj   
+   myLearn(Relation, AllExamples, Conj, LastVar),
+   remove(Examples, Conj, LastVar, RestExamples),           % Remove examples that match Conj   
    learn(Relation, RestExamples, Conjs).                    % Cover remaining examples 
 
 % learn_conj(Examples, Class, Conj):
 %    Conj is a list of attribute values satisfied by some examples of class Class and
 %    no other class
 
-myLearn(Relation, AllExamples, Conj) :-
-  learn_conj(AllExamples, Relation, Conj).
+myLearn(Relation, AllExamples, Conj, LastVar) :-
+  learn_conj(AllExamples, Relation, Conj, LastVar).
 
-myLearn(Relation, AllExamples, Conj) :-
+myLearn(Relation, AllExamples, Conj, LastVar) :-
   retract(condsLimit(X)),
   X1 is X + 1,
   assert(condsLimit(X1)),
-  myLearn(Relation, AllExamples, Conj).
+  myLearn(Relation, AllExamples, Conj, LastVar).
 
 %Check if there is any negative example in examples list connected with Relation
 %negativeExample(+Examples, +Relation)
@@ -57,30 +63,30 @@ negativeExample(Examples) :-
 
 %Podaj tylko Example dla relacji ktora cie interesuje!!!
 %learn_conj(+Examples, +Relation, -Conj).
-learn_conj(Examples, Relation, Conj) :-
+learn_conj(Examples, Relation, Conj, NewLastVar) :-
   functor(Relation, _, LastVar),
-  learn_conj(Examples, Relation, LastVar, [], Conj). 
+  learn_conj(Examples, Relation, LastVar, [], Conj, NewLastVar). 
 
 
 %zamkniecie rekurencji przepisuje zebrana liste na wyjscie.
-learn_conj(Examples, _, _, Conj, Conj)  :-
+learn_conj(Examples, _, NewLastVar, Conj, Conj, NewLastVar)  :-
    not(negativeExample(Examples)),
    !.   % There is no negative example
 
-learn_conj(Examples, Relation, LastVar, ConjCurrent, Conj)  :-
+learn_conj(Examples, Relation, LastVar, ConjCurrent, Conj, EndLastVar)  :-
    condsLimit(CondsLimit),
    length(ConjCurrent, Len), Len =< CondsLimit,
    choose_cond(Examples, Relation, LastVar, ConjCurrent, Cond, NewLastVar),    % choose one cond using score   
    filter(Examples, [Cond | ConjCurrent], NewLastVar, Examples1),   % filter Examples using what 
                                                                     % you already created
-   learn_conj(Examples1, Relation, NewLastVar, [Cond | ConjCurrent], Conj).   % 
+   learn_conj(Examples1, Relation, NewLastVar, [Cond | ConjCurrent], Conj, EndLastVar).   % 
 
 %choose_cond(+Examples, +Relation, +LastVar, +ConjCurrent, -Cond, -NewLastVar)
 choose_cond(Examples, Relation, LastVar, ConjCurrent, Cond, NewLastVar)  :-
    findall(CondCand/NewLastVar/Score, score(Examples, Relation, LastVar, ConjCurrent, CondCand, Score, NewLastVar), CondCands),
    qsort(CondCands, Sorted), 
-   member(Cond/NewLastVar/_, Sorted),
-   write('New Cond: '), write(Cond), write(' '), write(NewLastVar), nl.                   % Best score attribute value 
+   member(Cond/NewLastVar/_, Sorted).
+%   write('New Cond: '), write(Cond), write(' '), write(NewLastVar), nl.                   % Best score attribute value 
 
 % filter(Examples, Condition, Examples1):
 %    Examples1 contains elements of Examples that satisfy Condition
